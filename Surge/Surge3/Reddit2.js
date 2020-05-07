c091914b04cd2686f8cfce37bd7276aa9386919e
Cameron <Liujmiracle@163.com>
2020-05-07 20:30:14 +0800

    Update Reddit2.js

--- Surge/Surge3/Reddit2.js
+++ Surge/Surge3/Reddit2.js
@@ -19,8 +19,8 @@ if (body.data && body.data.subreddit && body.data.subreddit.posts && body.data.s
   body.data.subreddit.posts.edges = body.data.subreddit.posts.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
 }
 */
-if (body.data.home.post.edges) {
-  body.data.home.post.edges = body.data.home.post.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+if (body.data.home.posts.edges) {
+  body.data.home.posts.edges = body.data.home.posts.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
 }
 
 $done({
