e2a6173c0734e36841f036d3576fbd2c2cfecaa6
Cameron <Liujmiracle@163.com>
2020-05-07 17:31:49 +0800

    add Reddit2.js

---
+++ Surge/Surge3/Reddit2.js
@@ -0,0 +1,22 @@
+/*
+来源：https://raw.githubusercontent.com/xream/surge/master/Module/Reddit.sgmodule
+
+#!name=Reddit
+#!desc=Reddit官方客户端去除时间线Promoted广告
+#!system=ios
+
+[Script]
+Reddit.js = script-path=https://raw.githubusercontent.com/xream/surge/master/Script/Reddit.js,requires-body=true,type=http-response,pattern=^https?:\/\/gql\.reddit\.com
+
+[MITM]
+hostname = %APPEND% gql.reddit.com
+*/
+
+const body = JSON.parse($response.body)
+
+if (body.data && body.data.subreddit && body.data.subreddit.posts && body.data.subreddit.posts.edges) {
+  body.data.subreddit.posts.edges = body.data.subreddit.posts.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+}
+$done({
+  body: JSON.stringify(body)
+})
