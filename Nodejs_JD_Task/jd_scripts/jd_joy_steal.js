bcb47c57ff4d00f76732fc6c4419ef0e846ddb22
Cameron <Liujmiracle@163.com>
2021-03-12 20:42:45 +0800

    update

--- Nodejs_JD_Task/jd_scripts/jd_joy_steal.js
+++ Nodejs_JD_Task/jd_scripts/jd_joy_steal.js
@@ -583,11 +583,7 @@ function TotalBean() {
               $.isLogin = false; //cookie过期
               return
             }
-            if (data['retcode'] === 0) {
-              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
-            } else {
-              $.nickName = $.UserName
-            }
+            $.nickName = data['base'].nickname;
           } else {
             console.log(`京东服务器返回空数据`)
           }
